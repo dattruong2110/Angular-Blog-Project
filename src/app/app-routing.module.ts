f8561b7c5fae8fbd8ea995b2dc217b2eb3059ac0
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-04 11:15:24 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { PostsComponent } from './admin/dashboard/posts/posts.component';
+import { PostCreateComponent } from './admin/dashboard/posts/post-create/post-create.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { DefaultComponent } from './admin/default/default.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DefaultComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'posts', component: PostCreateComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
