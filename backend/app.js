aa33c5524a6f5bca4999f6e38d689cee4d58f5e6
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-04 17:41:24 +0700

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+// Middleware
+app.use(cors());
+app.options('*', cors);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Getting the posts
+app.use('/api/posts', (req, res, next) => {
+  const post = [
+    {
+      id: "1811060165",
+      title: "This is from server",
+      content: "Server first content",
+    },
+    {
+      id: "1811060166",
+      title: "This is from server",
+      content: "Server second content",
+    },
+  ];
+  res.status(200).json({
+    message: 'posts fetched successfully',
+    posts: post
+  });
+});
+
+// Posting data
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  res.status(200).json({
+    message: 'Post added successfully'
+  })
+})
+
+module.exports = app;
