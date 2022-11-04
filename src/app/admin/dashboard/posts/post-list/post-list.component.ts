18f7963642ebab297727edf8bed2dd4fe24d6f72
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-04 11:19:02 +0700

    Update post-list.component.ts

--- src/app/admin/dashboard/posts/post-list/post-list.component.ts
+++ src/app/admin/dashboard/posts/post-list/post-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { PostsService } from './../posts.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Post } from '../posts.model';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  private postsSub!: Subscription;
+
+  constructor(public postService: PostsService) { }
 
   ngOnInit(): void {
+    this.posts = this.postService.getPost();
+    this.postsSub = this.postService.getUpdate().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    })
   }
 
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe();
+  }
 }
