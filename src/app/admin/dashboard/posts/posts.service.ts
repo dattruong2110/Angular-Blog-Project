c9b13455be3bfa68c42ee924e8f9ebd266cec4d3
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-04 17:42:39 +0700

    Update posts.service.ts

--- src/app/admin/dashboard/posts/posts.service.ts
+++ src/app/admin/dashboard/posts/posts.service.ts
@@ -1,23 +1,36 @@
 import { Injectable } from '@angular/core'
 import { Post } from './posts.model';
 import { Subject } from "rxjs";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getUpdate() {
     return this.postUpdated.asObservable();
   }
 
-  addPost(title: string, content: string, dateTime: any) {
-    const post: Post = { title: title, content: content, dateTime: new Date() };
-    this.posts.push(post);
-    this.postUpdated.next([...this.posts]);
+  addPost( title: string, content: string, dateTime: any) {
+    const post: Post = { id: null, title: title, content: content, dateTime: new Date() };
+    this.http.post<{message: string}>
+    ('http://localhost:3000/api/posts', post)
+    .subscribe((response) => {
+      console.log(response.message)
+      this.posts.push(post);
+      this.postUpdated.next([...this.posts]);
+    })
   }
 
   getPost() {
-    return [...this.posts];
+    this.http.get<{message: string, posts: Post[]}>
+    ('http://localhost:3000/api/posts')
+    .subscribe(postData => {
+      this.posts = postData.posts;
+      this.postUpdated.next([...this.posts])
+    })
   }
 }
