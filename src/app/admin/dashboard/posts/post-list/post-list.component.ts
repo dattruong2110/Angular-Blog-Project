5cfe7c961cac704f3bbe035e47414ac8d6160c05
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-10 16:09:50 +0700

    Update post-list.component.ts

--- src/app/admin/dashboard/posts/post-list/post-list.component.ts
+++ src/app/admin/dashboard/posts/post-list/post-list.component.ts
@@ -1,8 +1,9 @@
+import { Subject } from 'rxjs';
+import { NgForm, NgModel, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from './../posts.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../posts.model';
 import { Subscription } from 'rxjs'
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-list',
@@ -11,35 +12,48 @@ import { NgForm } from '@angular/forms';
 })
 export class PostListComponent implements OnInit, OnDestroy {
 
+  public postForm!: FormGroup;
   posts: Post[] = [];
-  ngForm: NgForm;
   updateIndex!: any;
+  isEditEnabled: boolean = false;
   private postsSub!: Subscription;
+  private postUpdateNotifier: Subject<Post[]>;
 
-  constructor(public postService: PostsService) { }
+  constructor(public postService: PostsService) {
+    this.postUpdateNotifier = this.postService.getPostUpdatedNotifier();
+  }
 
   ngOnInit(): void {
+    if (this.postForm) {
+      this.postForm = new FormGroup({
+        title: new FormControl(['', Validators.required]),
+        content: new FormControl(['', Validators.required])
+      })
+    }
+
     this.postService.getPost();
-    this.postsSub = this.postService.getUpdate().subscribe((posts: Post[]) => {
-      this.posts = posts;
-    })
+
+    this.postUpdateNotifier.subscribe(updatedPosts => this.posts = updatedPosts);
   }
 
   ngOnDestroy(): void {
     this.postsSub.unsubscribe();
   }
 
-  // onEdit(posts: Post, idx: number) {
-  //   this.ngForm.controls['title'].setValue(posts.title);
-  //   this.ngForm.controls['content'].setValue(posts.content);
-  //   this.updateIndex = idx;
-  // }
+  onEdit(post: Post) {
+    // this.postForm.controls['title'].setValue(post.title);
+    // this.postForm.controls['content'].setValue(post.content);
+    // this.updateIndex = idx;
+    // this.isEditEnabled = true;
+    post.inEdit = true;
+  }
 
-  // updatePost() {
-  //   this.posts[this.updateIndex].title = this.ngForm.value.title;
-  //   this.posts[this.updateIndex].content = this.ngForm.value.content;
-  //   this.updateIndex = undefined;
-  // }
+  updatePost() {
+    this.posts[this.updateIndex].title = this.postForm.value.title;
+    this.posts[this.updateIndex].content = this.postForm.value.content;
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
+  }
 
   deletePost(idx: number) {
     this.posts.splice(idx, 1);
