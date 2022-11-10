8ee1620718f49aa19a3da08ab1704dff8d05e3ff
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-10 18:19:20 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { PostsComponent } from './admin/dashboard/posts/posts.component';
 import { PostCreateComponent } from './admin/dashboard/posts/post-create/post-create.component';
 
+import { ToastModule } from 'primeng/toast';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { SidebarModule } from 'primeng/sidebar';
@@ -21,7 +22,11 @@ import { PostListComponent } from './admin/dashboard/posts/post-list/post-list.c
 import { AccordionModule } from 'primeng/accordion';
 import { PanelModule } from 'primeng/panel';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { PostEditComponent } from './admin/dashboard/posts/post-edit/post-edit.component';
+import { PostCommentComponent } from './admin/dashboard/posts/post-comment/post-comment.component';
+import { PostComponent } from './admin/dashboard/posts/post/post.component';
 
 @NgModule({
   declarations: [
@@ -31,12 +36,16 @@ import { HttpClientModule } from '@angular/common/http';
     DashboardComponent,
     PostsComponent,
     PostCreateComponent,
-    PostListComponent
+    PostListComponent,
+    PostEditComponent,
+    PostCommentComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ToastModule,
     ButtonModule,
     RippleModule,
     SidebarModule,
@@ -47,6 +56,7 @@ import { HttpClientModule } from '@angular/common/http';
     AccordionModule,
     PanelModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
