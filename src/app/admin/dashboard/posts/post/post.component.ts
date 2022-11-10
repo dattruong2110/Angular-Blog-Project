5699129abca35cfa71fbed42b161732916746754
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-10 18:14:50 +0700

    Create post.component.ts

---
+++ src/app/admin/dashboard/posts/post/post.component.ts
@@ -0,0 +1,43 @@
+import { Subject } from 'rxjs';
+import { Subscription } from 'rxjs';
+import { FormGroup } from '@angular/forms';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Post } from '../posts.model';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  @Input() post: Post;
+
+  @Output() delete: EventEmitter<Post> = new EventEmitter();
+
+  public postForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onEdit() {
+    // this.postForm.controls['title'].setValue(post.title);
+    // this.postForm.controls['content'].setValue(post.content);
+    // this.updateIndex = idx;
+    // this.isEditEnabled = true;
+    this.post.inEdit = true;
+  }
+
+  updatePost() {
+    this.post.title = this.postForm.value.title;
+    this.post.content = this.postForm.value.content;
+    this.post.inEdit = false;
+  }
+
+  deletePost() {
+    this.delete.emit(this.post);
+  }
+
+}
