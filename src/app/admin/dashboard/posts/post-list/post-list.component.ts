944583f35787ad3466b116443bc6c4965ea3556f
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-11 17:45:52 +0700

    Update post-list.component.ts

--- src/app/admin/dashboard/posts/post-list/post-list.component.ts
+++ src/app/admin/dashboard/posts/post-list/post-list.component.ts
@@ -1,24 +1,30 @@
+import { MessageService } from 'primeng/api';
 import { Subject } from 'rxjs';
 import { NgForm, NgModel, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from './../posts.service';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, EventEmitter } from '@angular/core';
 import { Post } from '../posts.model';
 import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
-  styleUrls: ['./post-list.component.scss']
+  styleUrls: ['./post-list.component.scss'],
+  providers: [MessageService],
 })
 export class PostListComponent implements OnInit, OnDestroy {
 
   public postForm!: FormGroup;
+  @Input() post: Post;
+  @Input() delete = new EventEmitter<number>();
+
   posts: Post[] = [];
+  currentPost = this.posts[0];
   isEditEnabled: boolean = false;
   private postsSub!: Subscription;
   private postUpdateNotifier: Subject<Post[]>;
 
-  constructor(public postService: PostsService) {
+  constructor(public postService: PostsService, private messageService: MessageService) {
     this.postUpdateNotifier = this.postService.getPostUpdatedNotifier();
   }
 
@@ -39,4 +45,16 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.postsSub.unsubscribe();
   }
 
+  handleDeletePost(id: number) {
+    // this.delete.emit(this.post.id);
+    this.posts = this.posts.filter(post => {
+      return post.id !== id;
+    });
+
+    if (this.currentPost.id === id) {
+      this.currentPost = this.posts[0];
+    }
+
+    this.messageService.add({ severity: 'success', detail: 'Post has been deleted' });
+  }
 }
