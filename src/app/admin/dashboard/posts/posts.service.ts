fa109bdbca16c653ca37abfc88115410793f502f
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-18 16:46:56 +0700

    Update posts.service.ts

--- src/app/admin/dashboard/posts/posts.service.ts
+++ src/app/admin/dashboard/posts/posts.service.ts
@@ -1,7 +1,6 @@
-import { StorageService } from './../../../local-storage/local-storage.service';
 import { Injectable } from '@angular/core'
 import { Post } from './posts.model';
-import { Observable, of, ReplaySubject, Subject } from "rxjs";
+import { Observable, ReplaySubject, Subject } from "rxjs";
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
@@ -9,60 +8,55 @@ export class PostsService {
   private posts: Post[] = [];
   private postUpdated = new ReplaySubject<Post[]>();
 
-  private static postListStorageKey = 'Post';
+  private static readonly POST_KEY = 'POSTS';
 
-  constructor(private http: HttpClient, storageService: StorageService) { }
-
-  // getData(key: string): any {
-  //   return JSON.parse(localStorage.getItem(key)!);
-  // }
-
-  // setData(key: string, data: any): void {
-  //   return localStorage.setItem(key, JSON.stringify(data));
-  // }
+  constructor(private http: HttpClient) { }
 
   getUpdate() {
     return this.postUpdated.asObservable();
   }
 
-  // getPostList(): Observable<Post[]> {
-  //   return of(JSON.parse(window.localStorage.getItem(PostsService.postListStorageKey) || '[]'))
+  // addPost(post: Post) {
+  //   return this.http.post<{ message: string }>
+  //     ('http://localhost:3000/api/posts', post)
+  //     .subscribe((response) => {
+  //       this.posts.push(post);
+  //       // window.localStorage.setItem(PostsService.postListStorageKey, JSON.stringify(this.posts));
+  //       this.postUpdated.next([...this.posts]);
+  //     })
   // }
 
-  addPost(post: Post) {
-    return this.http.post<{ message: string }>
-      ('http://localhost:3000/api/posts', post)
-      .subscribe((response) => {
-        this.posts.push(post);
-        // window.localStorage.setItem(PostsService.postListStorageKey, JSON.stringify(this.posts));
-        this.postUpdated.next([...this.posts]);
-      })
+  saveList(): void {
+    window.localStorage.setItem(PostsService.POST_KEY, JSON.stringify(this.posts));
   }
 
-  // addPost(post: Post) {
-  //   this.getPostList().subscribe(posts => {
-  //     posts.push(post);
-  //     this.posts = posts;
-  //     window.localStorage.setItem(PostsService.postListStorageKey, JSON.stringify(this.posts));
-  //     this.postUpdated.next(this.posts);
+  addPost(post: Post) {
+    this.posts.push(post);
+    window.localStorage.setItem(PostsService.POST_KEY, JSON.stringify(this.posts));
+    this.postUpdated.next(this.posts);
+  }
+
+  deletePost(key: string) {
+    window.localStorage.removeItem(key);
+    this.getPost();
+    window.localStorage.setItem(PostsService.POST_KEY, JSON.stringify(this.posts));
+    // this.postUpdated.next(this.posts);
+  }
+
+  // getPost() {
+  //   this.http.get<{message: string, posts: Post[]}>
+  //   ('http://localhost:3000/api/posts')
+  //   .subscribe(postData => {
+  //     // this.posts = postData.posts.map(rawPost => ({ ...rawPost, comments: rawPost.comments || [] }));
+  //     this.posts = postData.posts;
+  //     this.postUpdated.next([...this.posts])
   //   })
   // }
 
   getPost() {
-    this.http.get<{message: string, posts: Post[]}>
-    ('http://localhost:3000/api/posts')
-    .subscribe(postData => {
-      // this.posts = postData.posts.map(rawPost => ({ ...rawPost, comments: rawPost.comments || [] }));
-      this.posts = postData.posts;
-      this.postUpdated.next([...this.posts])
-    })
+    this.posts = JSON.parse(window.localStorage.getItem(PostsService.POST_KEY) || '[]');
+    this.postUpdated.next(this.posts);
   }
-
-  // getPost() {
-  //   return this.getPostList().subscribe(posts => {
-  //     this.postUpdated.next(posts);
-  //   });
-  // }
 
   public getPostUpdatedNotifier() {
     return this.postUpdated;
