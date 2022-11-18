8e3affb54e66cf65e20d02e35dbd959ca95f0003
dattruong2110 <90130400+dattruong2110@users.noreply.github.com>
2022-11-18 16:46:46 +0700

    Update posts.model.ts

--- src/app/admin/dashboard/posts/posts.model.ts
+++ src/app/admin/dashboard/posts/posts.model.ts
@@ -1,10 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export class Post {
-  id: number;
+  id: number = uuidv4();
   title: string;
   content: string;
   dateTime: Date = new Date();
   inEdit: boolean = false;
   comments: string[] = [];
+  isLike: number = 0;
 
   constructor(title: string, content: string) {
     this.title = title;
